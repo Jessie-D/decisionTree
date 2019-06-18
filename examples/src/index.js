da44a1edae358a62898564db2bee424735bf3bfd
段文洁 <dwjjessie@gmail.com>
2019-06-18 11:00:12 +0800

    Updates

--- examples/src/index.js
+++
@@ -1,7 +0,0 @@
-/*** examples/src/index.js ***/
-import React from 'react'
-import { render } from 'react-dom'
-import DecisionTree from '../../src'
-import decisionTreeData from './decisionTreeData'
-const App = () => <DecisionTree decisionTreeData={decisionTreeData.data} />
-render(<App />, document.getElementById('root'))
